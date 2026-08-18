7611f54f9945a83b6a8822bacc05b7d906915a52
Soysakiaytuno <leonardhopst@gmail.com>
2026-08-17 21:02:44 -0400

    cambiando puerto 2

--- config/app.py
+++ config/app.py
@@ -7,4 +7,4 @@ def index():
     return render_template("index.html", dato="xd")
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
